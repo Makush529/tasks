cbe9d205d371536ae40817f409607815878dda3e
MakushevEvgenii <evmakush@gmail.com>
2025-08-24 19:59:49 +0300

    Готовый калькулятор

---
+++ src/cycle/tack001.java
@@ -0,0 +1,40 @@
+package cycle;
+
+import java.util.Scanner;
+
+public class tack001 {
+    public static void main(String[] args) {
+        /*Таблица умножения на консоль
+Напишите программу, которая выводит таблицу
+умножения размером n x n на экран,
+где значение n задаётся пользователем.
+
+Пример вывода таблицы для n=3:
+1   2   3
+2   4   6
+3   6   9
+*/
+        Scanner scan = new Scanner(System.in);
+        while (scan.hasNextInt()) {
+            int num = scan.nextInt();
+            for (int x = 1; x <= num; x++) {
+                System.out.print(" " + x + " ");
+            }
+            System.out.println();
+            for (int y = 1; y <= num; y++) {
+                System.out.print(y + " ");
+                for (int yy = 1; yy <= num; yy++) {
+                    System.out.print(yy*y + " ");
+
+                }
+                System.out.println();
+            }
+
+
+        }
+        scan.close();
+    }
+
+}
+
+
